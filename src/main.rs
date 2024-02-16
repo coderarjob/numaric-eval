b0aacf585e072ec4c74379ccb1d1bb43920f23ab
Arjob Mukherjee <mukherjeearjob@gmail.com>
2024-02-16 20:12:12 +0530

    Added exponent op and associativity property

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,84 @@ enum Operations {
     Addition,
     Multiply,
     Divide,
+    Exponent,
     CloseBracket,
+}
+
+enum OperatorAssociativity {
+    Left,
+    Right,
+}
+
+struct OperationProperties {
+    symbol: char,
+    associativity: OperatorAssociativity,
+    precedence: u32,
+}
+
+impl Operations {
+    const fn value(&self) -> OperationProperties {
+        match self {
+            Operations::NoOp => OperationProperties {
+                symbol: 'N',
+                associativity: OperatorAssociativity::Left,
+                precedence: 0,
+            },
+            Operations::OpenBracket => OperationProperties {
+                symbol: '(',
+                associativity: OperatorAssociativity::Left,
+                precedence: 1,
+            },
+            Operations::Subtraction => OperationProperties {
+                symbol: '-',
+                associativity: OperatorAssociativity::Left,
+                precedence: 2,
+            },
+            Operations::Addition => OperationProperties {
+                symbol: '+',
+                associativity: OperatorAssociativity::Left,
+                precedence: 2,
+            },
+            Operations::Multiply => OperationProperties {
+                symbol: '*',
+                associativity: OperatorAssociativity::Left,
+                precedence: 3,
+            },
+            Operations::Divide => OperationProperties {
+                symbol: '/',
+                associativity: OperatorAssociativity::Left,
+                precedence: 3,
+            },
+            Operations::Exponent => OperationProperties {
+                symbol: '^',
+                associativity: OperatorAssociativity::Right,
+                precedence: 4,
+            },
+            Operations::CloseBracket => OperationProperties {
+                symbol: ')',
+                associativity: OperatorAssociativity::Left,
+                precedence: 5,
+            },
+        }
+    }
+
+    fn parse(symbol: &str) -> Result<Operations, ()> {
+        match symbol {
+            "+" => Ok(Operations::Addition),
+            "-" => Ok(Operations::Subtraction),
+            "*" => Ok(Operations::Multiply),
+            "/" => Ok(Operations::Divide),
+            "(" => Ok(Operations::OpenBracket),
+            ")" => Ok(Operations::CloseBracket),
+            "^" => Ok(Operations::Exponent),
+            _ => Err(()),
+        }
+    }
 }
 
 impl Display for Operations {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let op_str = match self {
-            Self::Addition => "+".to_owned(),
-            Self::Subtraction => "-".to_owned(),
-            Self::Multiply => "*".to_owned(),
-            Self::Divide => "/".to_owned(),
-            Self::OpenBracket => "(".to_owned(),
-            Self::CloseBracket => ")".to_owned(),
-            Self::NoOp => "nop".to_owned(),
-        };
-        write!(f, "{}", op_str)
+        write!(f, "{}", self.value().symbol)
     }
 }
 
@@ -34,8 +97,8 @@ enum Tokens {
 }
 
 fn main() -> ExitCode {
+    let infix = "2 + 4 + 3";
     //let infix = "( 2 + 4 ) / 2 * 60";
-    let infix = "( 2 + 4 ) / 2 * 60";
 
     match postfix(infix) {
         Err(error) => {
@@ -83,22 +146,23 @@ fn postfix(eq: &str) -> Result<String, &'static str> {
 }
 
 fn addop(op: Operations, stack: &mut Vec<Operations>, out_queue: &mut Vec<String>) {
-    let top_op = stack.last().unwrap_or(&Operations::NoOp);
-    if *top_op > op {
-        out_queue.push(stack.pop().unwrap_or(Operations::NoOp).to_string());
+    let op_prop = op.value();
+    if let Some(top_op) = stack.last() {
+        if top_op.value().precedence > op_prop.precedence
+            || (top_op.value().precedence == op_prop.precedence
+                && matches!(op_prop.associativity, OperatorAssociativity::Left))
+        {
+            out_queue.push(stack.pop().unwrap().to_string());
+        }
     }
+
     stack.push(op);
 }
 
 fn detect_token(lexeme: &str) -> Tokens {
     let value = lexeme.parse::<u32>();
     let ret = match lexeme {
-        "+" => Tokens::Operation(Operations::Addition),
-        "-" => Tokens::Operation(Operations::Subtraction),
-        "*" => Tokens::Operation(Operations::Multiply),
-        "/" => Tokens::Operation(Operations::Divide),
-        "(" => Tokens::Operation(Operations::OpenBracket),
-        ")" => Tokens::Operation(Operations::CloseBracket),
+        c if Operations::parse(c).is_ok() => Tokens::Operation(Operations::parse(c).unwrap()),
         _ if value.is_ok() => Tokens::Number(value.unwrap()),
         x => Tokens::Unknown(x.to_owned()),
     };
@@ -108,8 +172,7 @@ fn detect_token(lexeme: &str) -> Tokens {
 #[test]
 fn same_precedence() {
     let infix = "2 + 4 + 3";
-    // TODO: Should be 2 4 + 3 +
-    assert_eq!(postfix(infix), Ok("2 4 3 + +".to_owned()));
+    assert_eq!(postfix(infix), Ok("2 4 + 3 +".to_owned()));
 }
 #[test]
 fn bodmas_order_test_1() {
@@ -126,6 +189,11 @@ fn bodmas_order_test_2() {
 #[test]
 fn bodmas_order_test_3() {
     let infix = "2 - 4 + 3 * 60 / 1";
-    // TODO: Should be 2 4 - 3 60 * 1 / +
-    assert_eq!(postfix(infix), Ok("2 4 3 60 1 / * + -".to_owned()));
+    assert_eq!(postfix(infix), Ok("2 4 - 3 60 * 1 / +".to_owned()));
+}
+
+#[test]
+fn exponent() {
+    let infix = "2 ^ 3 ^ 4";
+    assert_eq!(postfix(infix), Ok("2 3 4 ^ ^".to_owned()));
 }
